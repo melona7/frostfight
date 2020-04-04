0f8f7a794aa9f7be979bc8a13f94901821e1bbfa
logantid <logantid@umich.edu>
2020-04-04 17:22:43 -0600

    Initial commit

---
+++ assets/map1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="map1" tilewidth="32" tileheight="32" tilecount="792" columns="33">
+ <image source="centralMap.png" width="1084" height="786"/>
+</tileset>
