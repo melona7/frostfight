f938e1618d35fa8cb8d649cdb082c10aa20d365c
katevc <katevc@umich.edu>
2020-04-08 00:34:31 -0400

    updated entry screen for clarity (needs more)

--- scripts/splash.js
+++ scripts/splash.js
@@ -7,11 +7,20 @@ class splash extends Phaser.Scene {
     }
     create() {
         var bg = this.add.sprite(0,0,'background');
-		bg.setOrigin(0,0);
+        bg.setOrigin(0,0);
 
-		var text = this.add.text(100,100, 'Welcome to my game!');
+        let graphics = this.add.graphics();
+
+        graphics.fillStyle(0x000080, 1);
+
+        graphics.fillRect(350, 400, 115, 40);
+
+        var text = this.add.text(360,410, 'Start game');
         text.setInteractive({ useHandCursor: true });
         text.on('pointerdown', () => this.clickButton());
+
+        var title = this.add.text(100, 100, 'Frostfight: A Race Against Winter', {font: '30px Courier', fill: '#ffffff'});
+        var subtitle = this.add.text(10, 130, '(no lost freshman were harmed and/or frozen in the creation of this game)', {font: '18px Courier', fill: '#ffffff'});
 
 
     }
