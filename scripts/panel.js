491f0389f2d21a4bf778aa5abef0d42f2b865946
niezi <niezi@umich.edu>
2020-04-08 04:55:23 -0400

    title page

--- scripts/panel.js
+++ scripts/panel.js
@@ -45,46 +45,46 @@ class panel extends Phaser.Scene {
         // sound_button.events.onInputDown.add(mute_unmute, this);
         
         
-        //var text = this.add.text(50,50, 'scene2!');
-        let timeLeft = gameOptions.initialTime;
+        // //var text = this.add.text(50,50, 'scene2!');
+        // let timeLeft = gameOptions.initialTime;
 
-        // the energy container. A simple sprite
-        let heatContainer = this.add.sprite(130, 35, "heatcontainer").setScrollFactor(0);
+        // // the energy container. A simple sprite
+        // let heatContainer = this.add.sprite(130, 35, "heatcontainer").setScrollFactor(0);
 
-        // the energy bar. Another simple sprite
-        let heatBar = this.add.sprite(heatContainer.x + 11, heatContainer.y, "heatbar").setScrollFactor(0);
+        // // the energy bar. Another simple sprite
+        // let heatBar = this.add.sprite(heatContainer.x + 11, heatContainer.y, "heatbar").setScrollFactor(0);
 
-        // a copy of the energy bar to be used as a mask. Another simple sprite but...
-        let heatMask = this.add.sprite(heatBar.x, heatBar.y, "heatbar").setScrollFactor(0);
+        // // a copy of the energy bar to be used as a mask. Another simple sprite but...
+        // let heatMask = this.add.sprite(heatBar.x, heatBar.y, "heatbar").setScrollFactor(0);
 
-        // ...it's not visible...
-        heatMask.visible = false;
+        // // ...it's not visible...
+        // heatMask.visible = false;
 
-        // and we assign it as energyBar's mask.
-        heatBar.mask = new Phaser.Display.Masks.BitmapMask(this, heatMask);
+        // // and we assign it as energyBar's mask.
+        // heatBar.mask = new Phaser.Display.Masks.BitmapMask(this, heatMask);
 
-        // a boring timer.
-        let gameTimer = this.time.addEvent({
-            delay: 1000,
-            callback: function(){
-                //console.log("lmao");
-                timeLeft --;
+        // // a boring timer.
+        // let gameTimer = this.time.addEvent({
+        //     delay: 1000,
+        //     callback: function(){
+        //         //console.log("lmao");
+        //         timeLeft --;
 
-                // dividing enery bar width by the number of seconds gives us the amount
-                // of pixels we need to move the energy bar each second
-                let stepWidth = heatMask.displayWidth / gameOptions.initialTime;
+        //         // dividing enery bar width by the number of seconds gives us the amount
+        //         // of pixels we need to move the energy bar each second
+        //         let stepWidth = heatMask.displayWidth / gameOptions.initialTime;
 
-                // moving the mask
+        //         // moving the mask
                 
-                heatMask.x -= stepWidth;
-                //console.log(energyMask.x);
-                if(timeLeft == 0){
-                //stop somehow lol
-                }
-            },
-            callbackScope: this,
-            loop: true
-        });
+        //         heatMask.x -= stepWidth;
+        //         //console.log(energyMask.x);
+        //         if(timeLeft == 0){
+        //         //stop somehow lol
+        //         }
+        //     },
+        //     callbackScope: this,
+        //     loop: true
+        // });
 
       graphics = this.add.graphics();
 
@@ -111,46 +111,46 @@ class panel extends Phaser.Scene {
       //this.add.sprite(723, 35, "soundon");
       this.add.sprite(770, 35, "question");
     
-    //   //var text = this.add.text(50,50, 'scene2!');
-    //   let timeLeft = gameOptions.initialTime;
+      //var text = this.add.text(50,50, 'scene2!');
+      let timeLeft = gameOptions.initialTime;
 
-    //   // the energy container. A simple sprite
-    //   let heatContainer = this.add.sprite(130, 35, "heatcontainer").setScrollFactor(0);
+      // the energy container. A simple sprite
+      let heatContainer = this.add.sprite(130, 35, "heatcontainer").setScrollFactor(0);
 
-    //   // the energy bar. Another simple sprite
-    //   let heatBar = this.add.sprite(heatContainer.x + 11, heatContainer.y, "heatbar").setScrollFactor(0);
+      // the energy bar. Another simple sprite
+      let heatBar = this.add.sprite(heatContainer.x + 11, heatContainer.y, "heatbar").setScrollFactor(0);
 
-    //   // a copy of the energy bar to be used as a mask. Another simple sprite but...
-    //   let heatMask = this.add.sprite(heatBar.x, heatBar.y, "heatbar").setScrollFactor(0);
+      // a copy of the energy bar to be used as a mask. Another simple sprite but...
+      let heatMask = this.add.sprite(heatBar.x, heatBar.y, "heatbar").setScrollFactor(0);
 
-    //   // ...it's not visible...
-    //   heatMask.visible = false;
+      // ...it's not visible...
+      heatMask.visible = false;
 
-    //   // and we assign it as energyBar's mask.
-    //   heatBar.mask = new Phaser.Display.Masks.BitmapMask(this, heatMask);
+      // and we assign it as energyBar's mask.
+      heatBar.mask = new Phaser.Display.Masks.BitmapMask(this, heatMask);
 
-    //   // a boring timer.
-    //   let gameTimer = this.time.addEvent({
-    //       delay: 1000,
-    //       callback: function(){
-    //           //console.log("lmao");
-    //           timeLeft --;
+      // a boring timer.
+      let gameTimer = this.time.addEvent({
+          delay: 1000,
+          callback: function(){
+              //console.log("lmao");
+              timeLeft --;
 
-    //           // dividing enery bar width by the number of seconds gives us the amount
-    //           // of pixels we need to move the energy bar each second
-    //           let stepWidth = heatMask.displayWidth / gameOptions.initialTime;
+              // dividing enery bar width by the number of seconds gives us the amount
+              // of pixels we need to move the energy bar each second
+              let stepWidth = heatMask.displayWidth / gameOptions.initialTime;
 
-    //           // moving the mask
+              // moving the mask
               
-    //           heatMask.x -= stepWidth;
-    //           //console.log(energyMask.x);
-    //           if(timeLeft == 0){
-    //             //stop somehow lol
-    //           }
-    //       },
-    //       callbackScope: this,
-    //       loop: true
-    //   });
+              heatMask.x -= stepWidth;
+              //console.log(energyMask.x);
+              if(timeLeft == 0){
+                //stop somehow lol
+              }
+          },
+          callbackScope: this,
+          loop: true
+      });
       
     }
 
